69152f6472c6bddb98879d0e8aa1c0286395d8b7
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2023-08-31 13:09:54 +0200

    fix: delete old indices first

--- supabase/migrations/20230831110331_add_function_to_regenerate_embedding_indices.sql
+++ supabase/migrations/20230831110331_add_function_to_regenerate_embedding_indices.sql
@@ -4,11 +4,20 @@ CREATE OR REPLACE FUNCTION public.regenerate_embedding_indices()
 AS $function$
 BEGIN
 
-DO $$
-DECLARE
-    numRows INT;
+DO $$ 
+DECLARE 
+    index_name TEXT;
+	numRows INT;
 BEGIN
-    SELECT ROUND(COUNT(*) / 1000) INTO numRows FROM parsed_document_sections;
+	-- Delete old embedding indices first
+    FOR index_name IN 
+        SELECT indexname FROM pg_indexes WHERE indexname LIKE '%parsed_document_sections_embedding_idx%'
+    LOOP
+        EXECUTE 'DROP INDEX IF EXISTS ' || index_name;
+    END LOOP;
+
+	-- Generate new embedding indices
+	SELECT ROUND(COUNT(*) / 1000) INTO numRows FROM parsed_document_sections;
     EXECUTE 'CREATE INDEX ON parsed_document_sections USING ivfflat (embedding vector_l2_ops) WITH (lists = ' || numRows || ')';
 	EXECUTE 'CREATE INDEX ON parsed_document_sections USING ivfflat (embedding vector_ip_ops) WITH (lists = ' || numRows || ')';
 	EXECUTE 'CREATE INDEX ON parsed_document_sections USING ivfflat (embedding vector_cosine_ops) WITH (lists = ' || numRows || ')';
