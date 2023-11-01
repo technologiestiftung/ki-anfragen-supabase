0ce2ab958ef668c795347d39722f496afe7712d9
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2023-11-01 13:35:43 +0100

    feat: tables for "red number reports" (#21)
    * feat: migration to add new tables for red number reports
    
    * feat: split into processes, reports and report_sections
    
    * feat: tables for parsed red number reports and sections
    
    * feat: functions for finding missing embeddings and creating indices
    
    * fix: function parameters

---
+++ supabase/migrations/20231031102607_add_function_to_find_parsed_reports_with_incomplete_embeddings.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE FUNCTION public.find_parsed_reports_with_incomplete_embeddings()
+ RETURNS TABLE(id int, checksum text, meta jsonb, red_number_report_id int)
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+    RETURN QUERY
+    SELECT
+        pr.*
+    FROM
+        parsed_red_number_reports pr
+    WHERE
+        pr.id IN (
+            SELECT
+                pr1.id
+            FROM
+                parsed_red_number_reports pr1
+            LEFT JOIN parsed_red_number_report_sections prs ON pr1.id = prs.parsed_red_number_report_id
+            WHERE
+                prs.parsed_red_number_report_id IS NULL
+        );
+END;
+$function$
